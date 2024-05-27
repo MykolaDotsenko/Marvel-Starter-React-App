4a71baafe8b5aece545b4268dd1bb9a690f23ce3
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-05-27 14:04:07 +0300

    service connection

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,15 +1,38 @@
-
 class MarvelService {
-    getResources = async(url) => {
-       let res = await fetch(url);
+_apiBase = 'https://gateway.marvel.com:443/v1/public/'
+_apiKey = 'apikey=6f00fba70811bdd9daa3cf27662d5b52'
 
-       if(!res.ok){
-        throw new Error(`Error:Could not fetch ${url}, status: ${res.status} `);
-       }
-       return await res.json();
+    getResource = async (url) => {
+      let res = await fetch(url);
+  
+      if (!res.ok) {
+        throw new Error(`Error: Could not fetch ${url}, status: ${res.status}`);
+      }
+      return await res.json();
     }
-    getAllCharacters = () => {
-        return this.getResource();
+  
+    getAllCharacters = async () => {
+      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+return res.data.results.map(this._transformCharacter);
     }
 
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+      }
+
+
+
+      _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+      }
+  }
 }
+
+  export default MarvelService;
+  
