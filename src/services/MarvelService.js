7101b8ea4856cdc9de8651f4f02dfd7dd9bee7de
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-05-31 10:07:08 +0300

    heroes list

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,6 +25,7 @@ return res.data.results.map(this._transformCharacter);
 
       _transformCharacter = (char) => {
         return {
+          id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
